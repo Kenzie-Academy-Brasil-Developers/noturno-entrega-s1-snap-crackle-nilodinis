904b9cb5ef63e6b2f5f392f7aafd0ad0b18710bd
Nilo Dinis <nilodinis@hotmail.com>
2021-10-26 18:42:05 -0400

    Atividade com os códigos

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,18 @@
 // Seu código vai aqui
+function snapCrackle (maxValue){
+    let n=1
+    while (n<=maxValue){
+    if (maxValue%2 !=0){//número ímpar
+    console.log("Snap")}
+    if (maxValue/5==0) {//múltiplo de 5
+    if (maxValue%2 !=0) {//múltiplo de 5 e ímpar
+    console.log(", Crackle")}
+    console.log("Crackle")}
+    if (maxValue%2!=0 && maxValue/5==0){//par e  múltiplo de 5
+    console.log(", SnapCrackle")}
+    if (maxValue%2==0 && maxValue/5!=0){//par e não múltiplo de 5
+    console.log(maxValue)}
+    n++}
+    if(maxValue%n==0){//número primo
+    console.log(", Prime")
+    }}
