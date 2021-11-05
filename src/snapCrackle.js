2a6964374becf3ff2e41183829da3840f059f9a7
Nilo Dinis <nilodinis@hotmail.com>
2021-11-05 19:16:05 -0400

    Corrigido

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,18 +1,19 @@
-// Seu código vai aqui
 function snapCrackle (maxValue){
-    let n=1
-    while (n<=maxValue){
-    if (maxValue%2 !=0){//número ímpar
-    console.log("Snap")}
-    if (maxValue/5==0) {//múltiplo de 5
-    if (maxValue%2 !=0) {//múltiplo de 5 e ímpar
-    console.log(", Crackle")}
-    console.log("Crackle")}
-    if (maxValue%2!=0 && maxValue/5==0){//par e  múltiplo de 5
-    console.log(", SnapCrackle")}
-    if (maxValue%2==0 && maxValue/5!=0){//par e não múltiplo de 5
-    console.log(maxValue)}
-    n++}
-    if(maxValue%n==0){//número primo
-    console.log(", Prime")
-    }}
+    let n=""
+    for(let number=1; number<=maxValue;number++){
+        if (number%2 !==0){       //número ímpar
+            n += "Snap, "
+        }
+        else if(number%5 ===0){   //número múltiplo de 5
+            n += "Crackle, "
+        }
+        else if(number%2 !==0 && number%5 ===0){   //número ímpar e múltiplo de 5
+            n += "SnapCrackle, "
+        }
+        else if(number%2 ===0 && number%5 !==0){     //número nem ímpar e nem múltiplo de 5
+            n+= ", "  
+        }
+    }
+    console.log(n)
+}
+snapCrackle(12)
